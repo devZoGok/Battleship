caab51fc69c2ce98bd00e967fd99c428d9385285
devZoGok <nenkokeko@yandex.com>
2022-01-25 19:40:37 +0200

    implemented Lua manager and unit data manager

---
+++ Scripts/defConfigs.lua
@@ -0,0 +1,7 @@
+PATH = "/home/dominykas/c++/Battleship/Assets/"
+DEFAULT_TEXTURE = PATH .. "Textures/defaultTexture.jpg"
+
+numAppStates = 3
+numMaxBinds = 23
+numBinds = {12, 1, 23}
+numConfBinds = {0, 1, 13}
