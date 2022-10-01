83b45352f534b442a53ae5767d42e0350281f9e8
devZoGok <ekokekoa@yandex.com>
2022-10-01 16:59:34 +0300

    calculating cell position and determening impassibility

--- Assets/Scripts/mapExporter.py
+++ Assets/Scripts/mapExporter.py
@@ -5,9 +5,7 @@ LAND = 'LAND'
 SEA_LEVEL = 'SEA_LEVEL'
 UNDERWATER = 'UNDERWATER'
 
-def createWaterBody(wb):
-    waterBody = None
-    return waterBody
+waterBodies = D.objects['waterBodies'].children
 
 impassibleNodeVal = 65535
 passVal = 1
@@ -17,23 +15,66 @@ verts = list(ET.parse(fullFilename + '.xml').find('node').find('mesh').iter('ver
 
 def createCells(cellSize, mapSize, land):
     weights = []
-    cellsStr = 'nodes = \n\t\t{'
+    cellsStr = 'nodes = {'
     cellsStr += '\n\t\tsize = {x = ' + str(cellSize[0]) + ', y = ' + str(cellSize[1]) + ', z = ' + str(cellSize[2]) + '},\n'
     cellsStr += '\t\tcells = {\n\t\t'
 
     cellsByDim = [int(mapSize[0] / cellSize[0]), 1 if cellSize[1] == 0 else int(mapSize[1] / cellSize[1]), int(mapSize[2] / cellSize[2])]
     numCells = cellsByDim[0] * cellsByDim[1] * cellsByDim[2]
-    cellsStr += '{pos = '
+    cellsStr += 'pos = {'
     initPos = -.5 * np.array([mapSize[0] - cellSize[0], 0, mapSize[2] - cellSize[2]])
+    cellPos = []
 
     for i in range(numCells):
         xId = i % cellsByDim[0]
         yId = i / (cellsByDim[0] * cellsByDim[2])
         zId = (i / cellsByDim[0]) % cellsByDim[2]
-        cellPos = initPos + np.array([xId * cellSize[0], -yId * cellSize[1], zId * cellSize[2]])
-        cellsStr += '{x = ' + str(cellPos[0]) + ', y = ' + str(cellPos[1]) + ', z = ' + str(cellPos[2]) + '}'
+        cp = initPos + np.array([xId * cellSize[0], -yId * cellSize[1], zId * cellSize[2]])
+        cellPos.append(cp)
+        cellsStr += '{x = ' + str(cp[0]) + ', y = ' + str(cp[1]) + ', z = ' + str(-cp[2]) + '}'
         cellsStr += (', ' if i < numCells - 1 else '')
         
+    cellsStr += '},\nimpassible = {,\n'
+
+    for i in range(numCells):
+        xId = i % cellsByDim[0]
+        yId = i / (cellsByDim[0] * cellsByDim[2])
+        zId = (i / cellsByDim[0]) % cellsByDim[2]
+
+        waterbodyId = -1
+
+        for j in range(len(waterBodies)):
+            wb = waterBodies[j]
+            diffX = (abs(cellPos[i][0] - wb.location[0]) < .5 * wb.dimensions[0])
+            diffY = (abs(cellPos[i][1] - wb.location[2]) < .5 * wb.dimensions[2])
+            diffZ = (abs(cellPos[i][2] + wb.location[1]) < .5 * wb.dimensions[1])
+            
+            if diffX and diffZ:
+                waterbodyId = j
+                break
+
+        impassibleForSubs = False
+        impassibleForLandUnits = False
+    
+        for j in range(len(verts)):
+            if waterbodyId != -1:
+                waterbody = waterBodies[waterbodyId]
+
+            point = (float(verts[j].get('px')), float(verts[j].get('py')), float(verts[j].get('pz')))
+
+            withinX = (abs(point[0] - cellPos[i][0]) < 0.5 * cellSize[0])
+            withinY = ((abs(point[1] - cellPos[i][1]) < 0.5 * cellSize[1]) if land else True)
+            withinZ = (abs(point[2] - cellPos[i][2]) < 0.5 * cellSize[2])
+            pointWithin = (withinX and withinY and withinZ)
+
+            if not land and pointWithin and waterbodyId != -1:
+                impassibleForSubs = True
+
+            if land and pointWithin and (waterbodyId != -1 and verts[1] < waterbody.location.z):
+                impassibleForLandUnits = True
+
+        cellsStr += '{land = ' + str(impassibleForLandUnits).lower() + ', naval = ' + str(impassibleForSubs).lower()  + '}' + (', ' if i < numCells - 1 else '')
+
     cellsStr += '},\nweights = {\n'
 
     for i in range(numCells):
@@ -41,20 +82,6 @@ def createCells(cellSize, mapSize, land):
         yId = i / (cellsByDim[0] * cellsByDim[2])
         zId = (i / cellsByDim[0]) % cellsByDim[2]
 
-        for j in range(len(verts)):
-            pass
-            '''
-	        pointWithin = map->isPointWithin(i, waterbodyId, verts, cellSize, type == UnitType::UNDERWATER);
-	        WaterBody waterbody;
-
-	        if(waterbodyId != -1)
-	        	waterbody = map->getWaterBody(waterbodyId);
-
-	        impassibleForSeaUnits = (cellType == SEA_LEVEL && pointWithin && waterbodyId != -1 && verts.y > waterbody.pos.y);
-	        impassibleForSubs = (cellType == UNDERWATER && pointWithin && waterbodyId != -1);
-	        impassibleForLandUnits = (cellType == LAND && pointWithin && (waterbodyId != -1 && verts.y < waterbody.pos.y));
-	        terrainCellsImpassible[i] = (impassibleForSeaUnits || impassibleForSubs || impassibleForLandUnits);
-            '''
         for j in range(numCells):
             adjacent = False
 			
@@ -93,7 +120,6 @@ def createCells(cellSize, mapSize, land):
     return cellsStr
 
 
-waterBodies = D.objects['waterBodies'].children
 content = 'map = {\n\tnumWaterBodies = ' + str(len(waterBodies)) + ',\n'
 terrain = D.objects['terrain']
 content += '\timpassibleNodeValue = ' + str(impassibleNodeVal) + ',\n'
