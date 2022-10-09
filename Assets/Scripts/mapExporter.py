fixed cell position calculation bugs

Assets/Scripts/mapExporter.py
@@ -13,7 +13,7 @@ passVal = 1
 fullFilename = bpy.data.filepath[0 : bpy.data.filepath.rfind('.')]
 verts = list(ET.parse(fullFilename + '.xml').find('node').find('mesh').iter('vertdata'))
 
-def createCells(cellSize, mapSize, land):
+def createCells(cellSize, mapSize, height, land):
     weights = []
     cellsByDim = [int(mapSize[0] / cellSize[0]), 1 if cellSize[1] == 0 else int(mapSize[1] / cellSize[1]), int(mapSize[2] / cellSize[2])]
     numCells = cellsByDim[0] * cellsByDim[1] * cellsByDim[2]
@@ -22,16 +22,16 @@ def createCells(cellSize, mapSize, land):
     cellsStr += '\n\t\tsize = {x = ' + str(cellSize[0]) + ', y = ' + str(cellSize[1]) + ', z = ' + str(cellSize[2]) + '},\n'
     cellsStr += 'pos = {'
 
-    initPos = -.5 * np.array([mapSize[0] - cellSize[0], 0, mapSize[2] - cellSize[2]])
+    initPos = -.5 * np.array([mapSize[0] - cellSize[0], -2 * height, mapSize[2] - cellSize[2]])
     cellPos = []
 
     for i in range(numCells):
         xId = i % cellsByDim[0]
         yId = i / (cellsByDim[0] * cellsByDim[2])
-        zId = (i / cellsByDim[0]) % cellsByDim[2]
+        zId = (int(i / cellsByDim[0])) % cellsByDim[2]
         cp = initPos + np.array([xId * cellSize[0], -yId * cellSize[1], zId * cellSize[2]])
         cellPos.append(cp)
-        cellsStr += '{x = ' + str(cp[0]) + ', y = ' + str(cp[1]) + ', z = ' + str(-cp[2]) + '}'
+        cellsStr += '{x = ' + str(cp[0]) + ', y = ' + str(cp[1]) + ', z = ' + str(cp[2]) + '}'
         cellsStr += (', ' if i < numCells - 1 else '')
         
     cellsStr += '},\nimpassible = {\n'
@@ -133,8 +133,8 @@ terrDim = np.array([terrain.dimensions.x, terrain.dimensions.z, terrain.dimensio
 maxTurnAngles = [.1]
 sizes = [(14, 0, 14), (14, 6, 14)]
 
-content += '\t\tsize = {x = ' + str(terrDim[0]) + ', y = ' + str(terrDim[2]) + ', z = ' + str(terrDim[1]) + '},\n'
-content += '\t\t' + createCells(sizes[0], terrDim, True) + '\n'
+content += '\t\tsize = {x = ' + str(terrDim[0]) + ', y = ' + str(terrDim[1]) + ', z = ' + str(terrDim[2]) + '},\n'
+content += '\t\t' + createCells(sizes[0], terrDim, 0, True) + '\n'
 content += '\t},\n'
 content += '\tskybox = {left = "left.jpg", right = "right.jpg", up = "up.jpg", down = "down.jpg", front = "front.jpg", back = "back.jpg"},\n'
 content += '\twaterBodies = {\n'
@@ -147,7 +147,7 @@ for wb in waterBodies:
     content += '\t\t\tsize = {x = ' + str(waterDim[0]) + ', y = ' + str(waterDim[2]) + '},\n'
     content += '\t\t\trect = true,\n'
     content += '\t\t\talbedoMap = "water.jpg",\n'
-    content += '\t\t\t' + createCells(sizes[1], waterDim, False) + '\n'
+    content += '\t\t\t' + createCells(sizes[1], waterDim, wb.location.z, False) + '\n'
     content += '\t\t}' + (',' if i != len(waterBodies) - 1 else '') + '\n'
     i += 1
 