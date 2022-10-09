fixed bug of allowing units to move between land and water masses vector coord bugfixes

Assets/Scripts/mapExporter.py
@@ -144,7 +144,7 @@ for wb in waterBodies:
     content += '\t\t{\n'
     content += '\t\t\tpos = {x = ' + str(wb.location.x) + ', y = ' + str(wb.location.z) + ', z = ' + str(-wb.location.y) + '},\n'
     waterDim = np.array([wb.dimensions.x, terrDim[1], wb.dimensions.y]);
-    content += '\t\t\tsize = {x = ' + str(waterDim[0]) + ', y = ' + str(waterDim[2]) + '},\n'
+    content += '\t\t\tsize = {x = ' + str(waterDim[0]) + ', y = 0, z = ' + str(waterDim[2]) + '},\n'
     content += '\t\t\trect = true,\n'
     content += '\t\t\talbedoMap = "water.jpg",\n'
     content += '\t\t\t' + createCells(sizes[1], waterDim, wb.location.z, False) + '\n'