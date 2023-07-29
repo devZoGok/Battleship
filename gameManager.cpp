88a2daa78632e4729d14bd7d1f836262c85c0030
devZoGok <nenkokeko@yandex.com>
2023-07-29 09:52:03 +0300

    using main() arguments to set the game dir

--- gameManager.cpp
+++ gameManager.cpp
@@ -26,26 +26,26 @@ namespace battleship{
 		return gameManager;
 	}
 
-    GameManager::GameManager() {
-		LuaManager *luaManager = LuaManager::getSingleton();
-		string pathBase = "../Assets/Scripts/";
-		luaManager->buildScript(vector<string>{pathBase + "defPaths.lua"});
-		path = luaManager->getString("PATH");
-    }
+	void GameManager::start(string gameDir) {
+		path = gameDir + "Assets/";
 
-    GameManager::~GameManager() {}
-
-	void GameManager::start() {
 		LuaManager *luaManager = LuaManager::getSingleton();
-		luaManager->buildScript(vector<string>{path + "Scripts/main.lua"});
-		luaManager->buildScript(configData::scripts);
+		luaManager->executeCode("PATH = \"" + path + "\";");
+
+		vector<string> files = configData::scripts;
+		files.emplace(files.begin(), "Scripts/main.lua");
+
+		for(string &f : files)
+			f = path + f;
+
+		luaManager->buildScript(files);
 
 		string ind1 = "graphics", ind2 = "resolution";
 		width = luaManager->getIntFromTable(ind1, vector<Index>{Index(ind2), Index("x")});
 		height = luaManager->getIntFromTable(ind1, vector<Index>{Index(ind2), Index("y")});
 
 		Root *root = Root::getSingleton();
-		root->start(width, height, "../../vb01/", "Battleship");
+		root->start(width, height, path + "../external/vb01/", "Battleship");
 
 		stateManager = new StateManager();
     	inputManager = new InputManager(stateManager, root->getWindow());
