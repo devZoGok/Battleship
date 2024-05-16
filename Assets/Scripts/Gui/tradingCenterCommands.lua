b4891fd890695d0f6618f84a3a206cd57270c8af
devZoGok <nenkokeko@yandex.com>
2024-05-16 19:47:09 +0300

    removed imagepath fields

--- Assets/Scripts/Gui/tradingCenterCommands.lua
+++ Assets/Scripts/Gui/tradingCenterCommands.lua
@@ -8,7 +8,6 @@ gui = {
 		pos = {x = res.x - Size.x, y = res.y - Size.y},
 		size = sz,
 		name = 'Buy ref',
-		imagePath = '',
 		guiType = GuiType.BUTTON,
 		buttonType = ButtonType.BUY_REFINEDS,
 		trigger = 73,
@@ -16,7 +15,6 @@ gui = {
 	{
 		pos = {x = res.x - 2 * Size.x, y = res.y - Size.y},
 		size = sz,
-		imagePath = '',
 		name = 'Sell ref',
 		guiType = GuiType.BUTTON,
 		buttonType = ButtonType.SELL_REFINEDS,
@@ -25,7 +23,6 @@ gui = {
 	{
 		pos = {x = res.x - Size.x, y = res.y - 2 * Size.y},
 		size = sz,
-		imagePath = '',
 		name = 'Buy rsch',
 		guiType = GuiType.BUTTON,
 		buttonType = ButtonType.BUY_RESEARCH,
@@ -34,7 +31,6 @@ gui = {
 	{
 		pos = {x = res.x - 2 * Size.x, y = res.y - 2 * Size.y},
 		size = sz,
-		imagePath = '',
 		name = 'Sell rsch',
 		guiType = GuiType.BUTTON,
 		buttonType = ButtonType.SELL_RESEARCH,
